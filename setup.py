691ab84a79a7003854a1a169abd0e40d040b126f
Vadim Kovrigin <kovrigin.dev@gmail.com>
2019-09-10 18:49:56 +0300

    v0.1.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ EXTRAS_REQUIRE = {
 
 setuptools.setup(
     name='universa',
-    version='0.0.7',
+    version='0.1.0',
     author='Vadim Kovrigin',
     author_email='kovrigin.dev@gmail.com',
     description='Python package to access Universa API from python.',
